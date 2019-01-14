3a805dc446d6be4e2b2a7e74713f661d25682934
Mohan Cm <mohanmanjappa@gmail.com>
2019-01-14 16:56:25 +0530

    feat : Implement BottomNavigationBar

--- app/src/main/java/com/anyemi/omrooms/MainActivity.java
+++ app/src/main/java/com/anyemi/omrooms/MainActivity.java
@@ -2,10 +2,15 @@ package com.anyemi.omrooms;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
 
     public static Intent getStartIntent(Context context) {
 
@@ -17,5 +22,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.action_home:
+
+                    case R.id.action_saved:
+
+                    case R.id.action_bookings:
+
+                    case R.id.action_account:
+
+                }
+                return true;
+            }
+        });
     }
+
 }
