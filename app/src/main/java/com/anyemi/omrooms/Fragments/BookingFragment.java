c3a0c93754ce9ee2d21099566f8ab1b0db610811
Praful Nayak <prafulnayak1988@gmail.com>
2019-01-18 14:06:33 +0530

    Child fragment inside Booking fragment and profile fragment sample

--- app/src/main/java/com/anyemi/omrooms/Fragments/BookingFragment.java
+++ app/src/main/java/com/anyemi/omrooms/Fragments/BookingFragment.java
@@ -3,7 +3,9 @@ package com.anyemi.omrooms.Fragments;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,12 +13,27 @@ import android.view.ViewGroup;
 import com.anyemi.omrooms.R;
 
 public class BookingFragment extends Fragment {
-
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 //        View view = inflater.inflate(R.layout.fragment_home, container, false);
         return inflater.inflate(R.layout.fragment_booking, container, false);
 //        return inflater.inflate(R.layout.fragment_home,null);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        tabLayout = view.findViewById(R.id.tab_layout);
+        viewPager = view.findViewById(R.id.view_pager);
+        //child fragment inside fragment
+        BookingHistoryAdapter adapter = new BookingHistoryAdapter(getChildFragmentManager());
+        adapter.addFragment(new BookingHistoryFragment(),"Booking");
+        adapter.addFragment(new BookingHistoryFragment(),"Cancelled");
+        adapter.addFragment(new BookingHistoryFragment(),"Stayed");
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+
     }
 }
